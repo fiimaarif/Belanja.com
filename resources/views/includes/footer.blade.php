002dd3bc27c647e563eae2fa40ae453d40e2931d
fiimaarif <fiimaarif44@gmail.com>
2022-02-26 16:04:41 +0700

    first commit

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,25 @@
+<footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <div class="pt-4" style="color: #6c757d">
+              Metode Pembayaran
+            </div>
+            <div class="pt-1 pb-2">
+              <img src="/images/bri.png" class="mr-2" style="width: 50px;" >
+              <img src="/images/bni.png" class="mr-2" style="width: 50px;" >
+              <img src="/images/bca.png" class="mr-2" style="width: 50px;" >
+              <img src="/images/mandiri.png" class="mr-2" style="width: 50px;" >
+              <img src="/images/gopay.png" class="mr-2" style="width: 50px;" >
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <p class="pt-4 pb-2">
+              2021 Copyright Belanja.com. All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
